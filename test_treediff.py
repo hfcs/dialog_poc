00d1eacfc2b8ab7246d745f376e05b5e75934a65
hfcs <hfcs@users.noreply.github.com>
2024-01-11 00:54:39 +0800

    add the tree diff test case

---
+++ test_treediff.py
@@ -0,0 +1,45 @@
+import sys
+import math
+import pandas as pd
+from anytree import Node, PreOrderIter, RenderTree, AsciiStyle
+from anytree.exporter import MermaidExporter
+from chatdialogflow import BaseNode, ButtonCaseIdListNode, JumpToNode, LeafNode, DialogFlowForest, WorkspaceCaseId, NodeToDrlRulePrinterSingleton, isBlank, InputTableValidator, ClonedDialogTree
+
+myValidator1 = InputTableValidator()
+myValidator2 = InputTableValidator()
+
+
+df1 = pd.read_excel('faq.xlsx') # or read_sql()
+df2 = pd.read_excel('faq_mod.xlsx')
+
+myValidator1.preScanPass(df1)
+myValidator1.validationPass(df1)
+myValidator2.preScanPass(df2)
+myValidator2.validationPass(df2)
+
+###############################################################################
+# main pass building tree
+# loop through the rows using iterrows()
+
+dialogFlowForest1 = DialogFlowForest()
+dialogFlowForest1.buildForrestFromInputTable(df1)
+
+dialogFlowForest2 = DialogFlowForest()
+dialogFlowForest2.buildForrestFromInputTable(df2)
+
+
+clonedTree1 = ClonedDialogTree(dialogFlowForest1.getTreeRootsList()[0])
+clonedTree2 = ClonedDialogTree(dialogFlowForest2.getTreeRootsList()[0])
+
+clonedTree1.markMyDelta(clonedTree2)
+clonedTree2.markMyDelta(clonedTree1)
+
+def nodefunc(node):
+    if node.isComparedSame():
+        return '["%s"]' % (node.name)
+    else:
+        return '[\\"%s"/]' % (node.name)        
+
+clonedTree1.printMermaid(sys.stdout)
+print("***********************")
+clonedTree2.printMermaid(sys.stdout)
